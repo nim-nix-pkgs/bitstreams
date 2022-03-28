52f68e23c1b99c6059bf917894955d5094625105
hugosenari <hugosenari@gmail.com>
2022-03-27 21:48:14 -0300

    chore: re index bitstreams

--- main/flake.nix
+++ main/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-bitstreams-main.ref   = "refs/heads/main";
   inputs.src-bitstreams-main.owner = "sealmove";
   inputs.src-bitstreams-main.repo  = "bitstreams";
-  inputs.src-bitstreams-main.dir   = "";
   inputs.src-bitstreams-main.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
