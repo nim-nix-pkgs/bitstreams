e804084a932c55e479ad2e89fc0542a69d191848
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:08 -0300

    chore: re index bitstreams

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."bitstreams-main".dir   = "main";
+  inputs."bitstreams-main".owner = "nim-nix-pkgs";
+  inputs."bitstreams-main".ref   = "master";
+  inputs."bitstreams-main".repo  = "bitstreams";
+  inputs."bitstreams-main".type  = "github";
+  inputs."bitstreams-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bitstreams-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
